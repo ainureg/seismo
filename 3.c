19846c98c99b0c27bde183804ef2728fa2dcee47
A.Karimov <ainureg@ya.ru>
2015-05-06 04:07:39 +0300

    wrong but prettier

--- 3.c
+++ 3.c
@@ -3,54 +3,60 @@
 #include <stdlib.h>
 #include <complex.h>
 #include <math.h>
-#define N 200//размер расчетной прямой
-#define w 5 //частота
+#define N 400 //размер расчетной прямой
+//#define w 5 //частота
 #define tmax 10 // нужный момент времени
+//#define mu 10
 
-
-void EMatr(int n, double** );
-double gf(double m);
-double cf(double x);
-void FMatr(int n,double h, double* );	
-void DMatr(int n, double** );
-void KMatr(int n, double** );
-void MMatr(int n, double** );
+double mu=10;
+double const w = 5;
+double h=(double) 2/N;//шаг по прямой;
+complex sigm (double x);
+complex prm(double x);
+void EMatr(int n, complex** );
+complex gf(complex m);
+complex cf(complex x);
+void FMatr(int n, complex* );	
+void DMatr(int n, complex** );
+void KMatr(int n, complex** );
+void MMatr(int n, complex** );
 
 int main(){
-    double h=(double) 1/N;//шаг по прямой;
+    
+    
     double t=h/2 ;//шаг по времени
     double Nt=tmax/t ;// шагов по времени
     int k;
     k=(int)pow(2,(int)ceil(log(N-1)/log(2)));
     int i,j;
-    double **K, **D, **M, *F;
+    complex **K, **D, **M, *F;
     int n;
     complex *a,*b,*c,*u;
     complex** A;
     n=(k+1);
-    K = (double**)malloc((n) * sizeof(double*));
-    M = (double**)malloc((n) * sizeof(double*));
-    F = (double*)malloc((n)  * sizeof(double));
-    D = (double**)malloc((n) * sizeof(double*));
+    K = (complex**)malloc((n) * sizeof(complex*));
+    M = (complex**)malloc((n) * sizeof(complex*));
+    F = (complex*)malloc((n)  * sizeof(complex));
+    D = (complex**)malloc((n) * sizeof(complex*));
     A = (complex**)malloc((n)* sizeof(complex*));
     if ((!A)&(!D)&(!M)&(!F)&(!D)) {
  	printf("error");
  	exit(1);
  }
     for (i=0;i<n;i++){
-        K[i]=(double*)malloc((n) * sizeof(double));
-        M[i]=(double*)malloc((n) * sizeof(double));
-        D[i]=(double*)malloc((n) * sizeof(double));
+        K[i]=(complex*)malloc((n) * sizeof(complex));
+        M[i]=(complex*)malloc((n) * sizeof(complex));
+        D[i]=(complex*)malloc((n) * sizeof(complex));
         A[i]=(complex*)malloc((n) * sizeof(complex));
     }
     
     //EMatr(n,E);
-    FMatr(N,h,F);	
+    FMatr(N, F);	
     DMatr(N, D);
     KMatr(N, K);
     MMatr(N, M);
 
-    
+    //multiply matrices by h 
     for (i=0;i<N;i++){
         for (j=0;j<N;j++){
             K[i][j]=K[i][j]/h;
@@ -139,26 +145,23 @@ int main(){
 return 0 ;
 }
 
-void DMatr(int n, double** M){
+void DMatr(int n, complex** M){
     M[n-1][n-1]=1/cf(1);
     }
-void FMatr(int n, double h, double* M){
+void FMatr(int n, complex* M){
+    double w=5;
     M[0]=w*w*h*gf(0.5)/6+1/h;
     }
-double gf(double m){
-    //double h=1/N;//шаг по прямой
-    return ( ( 1/cf(m/N) ) / cf(m/N) );
-    }
-double cf(double x){
+complex cf(complex x){
     return (0.1 +3.6*(x-0.5)*(x-0.5));
     }
-void EMatr(int n,double** M){
+void EMatr(int n,complex** M){
     int i;
     for (i=0;i<n;i++){
         M[i][i]=1;
         }
     }
-void KMatr(int n, double** M){
+void KMatr(int n, complex** M){
     int i;
     for (i=0;i<(n-1);i++){
         M[i][i]=2;
@@ -167,19 +170,35 @@ void KMatr(int n, double** M){
 }
     M[n-1][n-1]=1;
     }
-void MMatr(int n, double **M ){      
+void MMatr(int n, complex **M ){      
     int i;
+    double w=5;
+    double h=1/N;
     for (i=0;i<(n-1);i++){
-        M[i][i]=2*(gf(i+0.5)+gf(i+1.5));
-        M[i+1][i]=gf(i+1.5);
-        M[i][i+1]=gf(i+1.5);
+        M[i][i]=2*(gf(i+0.5)*prm(i+0.5)+gf(i+1.5)*prm(i+1.5));
+        M[i+1][i]=gf(i+1.5)*prm(i+1.5);
+        M[i][i+1]=gf(i+1.5)*prm(i+1.5);
         }
 
-    M[n-1][n-1]=2*(gf(n-0.5));
+    M[n-1][n-1]=2*(gf(n-0.5))*prm(n-0.5);
     }
-double sigm(x, mu ){
+complex prm ( double x){
+    return (w - I * sigm(x))*h/w/2;
+    }    
+complex gf(complex m){
+    return ( ( 1/cf(m/N) ) / cf(m/N) );
+    }    
+complex sigm(double x){
 if (x <= 1)
     return  0;
 else
-    return mu * (x - 1)*(x-1);
+    return mu*(x-1)*(x-1);
     }
+
+    
+//int solve (int n, T ∗top, T ∗mid, T ∗bot, T ∗b, T ∗x){
+    
+    
+  //  }
+
+    
